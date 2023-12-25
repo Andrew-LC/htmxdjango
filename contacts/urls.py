91429ed8ba95ba8173f83d3b70fed093443c5c2a
Andrew-LC <smartandrew2003@gmail.com>
2023-12-25 21:31:30 +0530

    added- edit,view,delete

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("new", views.new, name='new'),
+    path("<int:contact_id>", views.view, name='view'),
+    path("<int:contact_id>/edit", views.edit, name='edit'),
+    path("<int:contact_id>/delete", views.delete, name='delete'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.http import HttpResponse
 from django.template import loader
 from .models import Contact
+from .forms import NewContact
 
 def index(request):
    query = request.GET.get("q")
@@ -16,3 +18,63 @@ def index(request):
    }
    return HttpResponse(template.render(context, request))
     
+
+def new(request):
+    if request.method == "POST":
+        form = NewContact(request.POST)
+        if form.is_valid():
+            new_contact = Contact(
+                first=form.cleaned_data['first_name'],
+                last=form.cleaned_data['last_name'],
+                phone=form.cleaned_data['phone']
+            )
+            new_contact.save()
+            messages.success(request, "Successfully created a new contact")
+            return redirect("/contacts")
+        else:
+            messages.error(request, "Form validation error")
+    else:
+        form = NewContact()
+
+    template = loader.get_template("contacts/new.html")
+    return HttpResponse(template.render({"form": form}, request))
+
+
+def view(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    print(contact)
+    template = loader.get_template("contacts/view.html")
+    return HttpResponse(template.render({"contact": contact}, request))
+
+
+def edit(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+
+    if request.method == "POST":
+       form = NewContact(request.POST)
+       if form.is_valid():
+          contact.first = form.cleaned_data['first_name']
+          contact.last = form.cleaned_data['last_name']
+          contact.phone = form.cleaned_data['phone']
+          contact.save()
+          messages.success(request, "Successfully updated contact")
+          return redirect("/contacts")
+       else:
+          messages.error(request, "Form validation error")
+    else:
+       form = NewContact()
+
+    template = loader.get_template("contacts/edit.html")
+    return HttpResponse(template.render({"form": form, "contact": contact}, request))
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id)
+
+    if request.method == "POST":
+        contact.delete()
+        messages.success(request, "Successfully deleted contact")
+        return redirect("/contacts")
+    else:
+        messages.error(request, "Invalid request method for contact deletion")
+        return redirect("/contacts")
